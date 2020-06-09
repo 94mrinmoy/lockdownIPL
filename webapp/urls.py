859566351dff8fb14bf600a7ef9fd5cdbed5b724
Mrinmoy Das <94mrinmoydas@gmail.com>
2020-06-10 01:54:11 +0530

    6102020

--- webapp/urls.py
+++ webapp/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.views.generic import TemplateView
 
 from . import views
 
@@ -7,5 +8,12 @@ urlpatterns = [
     path('points/', views.points, name='points'),
     path('adminportal/', views.adminPortal, name='adminPortal'),
     path('adminportal2/', views.adminPortal2, name='adminPortal2'),
+    path(
+        'sw.js',
+        TemplateView.as_view(template_name='sw.js', content_type='application/javascript'),
+        name='sw.js',
+    ),
+
+    # path('static/a', views.index, name='index'),
 
 ]
